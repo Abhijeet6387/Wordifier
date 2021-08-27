b0e700c034db82cdc1308611f7e788fb695ce3af
Abhijeet Shankar Mishra <abhijeet.s.mishra@gmail.com>
2021-08-27 19:37:32 +0530

    about updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
@@ -8,6 +9,7 @@ function App() {
       <Navbar title="TextUtils" />
       <div className="container-fluid">
         <TextForm heading="Enter text to analyze" />
+        {/* <About /> */}
       </div>
     </>
   );
