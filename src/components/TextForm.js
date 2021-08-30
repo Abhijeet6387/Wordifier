c6e0afe401632a2beed8b4674b6f17384d51b672
Abhijeet Shankar Mishra <abhijeet.s.mishra@gmail.com>
2021-08-30 10:27:57 +0530

    copy text button bug fixed

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -30,6 +30,7 @@ export default function TextForm(props) {
     var text = document.getElementById("myBox");
     text.select();
     navigator.clipboard.writeText(text.value);
+    document.getSelection().removeAllRanges();
     props.showAlert("Copied to clipboard!", "success");
   };
 
